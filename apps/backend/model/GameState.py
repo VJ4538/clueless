c5f1c74e929271be4a60e35092b86ebba86d22d5
Steven Hetrick <sh6765@cs.ship.edu>
2025-03-14 20:50:54 -0400

    refactor

--- apps/backend/model/GameState.py
+++ apps/backend/model/GameState.py
@@ -1,24 +1,14 @@
-from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import List
 
-router = APIRouter()
+class Player(BaseModel):
+    name: str
+    character: str
+    current_room: str
+    has_made_suggestion: bool
 
-# Define the structure of your game state object
 class GameState(BaseModel):
     current_turn: str
-    players: List[str]
+    players: List[Player]
     rooms: List[str]
     suggestions: List[str]
-
-# Example game state
-current_game_state = {
-    "current_turn": "Player 1",
-    "players": ["Player 1", "Player 2", "Player 3"],
-    "rooms": ["Living Room", "Kitchen", "Ballroom"],
-    "suggestions": ["Player 1 suggested: Colonel Mustard in the Library with the Revolver"],
-}
-
-@router.get("/game/state", response_model=GameState)
-async def get_game_state():
-    return current_game_state
